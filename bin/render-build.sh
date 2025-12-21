debc4d5cf38ec6380908848a10ccc19772d89034
osama-moha <abdulmalikmoha71@gmail.com>
2025-12-21 20:44:59 +0300

    Reset admin password

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,4 @@ bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
 ./bin/rails db:migrate
+./bin/rails runner "SystemAdmin.find_by(email: 'admin@urbaneye.co.ke')&.update(password: 'Reset123!', password_confirmation: 'Reset123!')"
