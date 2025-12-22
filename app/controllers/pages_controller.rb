e004d21c706c1c350d730feba76c8e6b33540493
osama-moha <abdulmalikmoha71@gmail.com>
2025-12-22 12:53:20 +0300

    Add Services Portfolio page like Vega Security

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def services
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "pages/services"
   # --- 1. System Admin & Login ---
   devise_for :system_admins
 
